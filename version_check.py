c9bf6162defb94603d415a1e4172b637c6eab55a
ryeomyoung2 <ryeomyoung2@gmail.com>
2022-11-07 12:16:37 +0900

    update

---
+++ version_check.py
@@ -0,0 +1,11 @@
+import pyspark
+import numpy as np
+import pandas as pd 
+import matplotlib as mpl
+import seaborn as sns
+
+print(pyspark.__version__)
+print(np.__version__)
+print(pd.__version__)
+print(mpl.__version__)
+print(sns.__version__)
